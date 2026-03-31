30001e35f20b029942de9879a8560bb4ec5730d2
Neila <neila.asynur@gmail.com>
2026-03-31 14:43:04 +0700

    modul 4, post test

---
+++ PostTest/src/Mahasiswa.java
@@ -0,0 +1,11 @@
+public class Mahasiswa {
+    String nama;
+    int umur;
+    String jurusan;
+
+    public Mahasiswa(String nama, int umur, String jurusan) {
+        this.nama = nama;
+        this.umur = umur;
+        this.jurusan = jurusan;
+    }
+}
---
+++ PostTest/src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Mahasiswa mhsBaru = new Mahasiswa("Neila", 20, "Ilmu Komputer");
+    }
+}
